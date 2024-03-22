4789568b33e25a0a49c73f362aa73d7ece85aeaf
Igor Oliveira <igvini010@gmail.com>
2024-03-22 15:07:53 -0300

    Igor: Adicionando primeiro codigo

---
+++ src/main/java/com/travelplan/TravelPlanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.travelplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPlanBackendApplication.class, args);
+	}
+
+}
